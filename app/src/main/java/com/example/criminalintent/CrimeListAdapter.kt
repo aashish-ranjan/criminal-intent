6a87f0d0b02824b813dd0e712e3087082c5fe690
aashish-ranjan <aashishranjan2011@gmail.com>
2023-09-21 07:39:55 +0530

    display only normal crime in rv

--- app/src/main/java/com/example/criminalintent/CrimeListAdapter.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListAdapter.kt
@@ -3,34 +3,21 @@ package com.example.criminalintent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView.Adapter
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.criminalintent.databinding.ListItemNormalCrimeBinding
-import com.example.criminalintent.databinding.ListItemSeriousCrimeBinding
 import com.example.criminalintent.model.Crime
 
-class CrimeListAdapter(private val crimeList: List<Crime>): Adapter<ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+class CrimeListAdapter(private val crimeList: List<Crime>): Adapter<NormalCrimeHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NormalCrimeHolder {
         val inflater = LayoutInflater.from(parent.context)
-        return if (viewType == 0) {
-            val binding = ListItemNormalCrimeBinding.inflate(inflater, parent, false)
-            NormalCrimeHolder(binding)
-        } else {
-            val binding = ListItemSeriousCrimeBinding.inflate(inflater, parent, false)
-            SeriousCrimeViewHolder(binding)
-        }
+        val binding = ListItemNormalCrimeBinding.inflate(inflater, parent, false)
+        return NormalCrimeHolder(binding)
     }
 
     override fun getItemCount(): Int = crimeList.size
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: NormalCrimeHolder, position: Int) {
         val crime = crimeList[position]
-        if (holder is NormalCrimeHolder) holder.bind(crime)
-        else if (holder is SeriousCrimeViewHolder) holder.bind(crime)
-    }
-
-    override fun getItemViewType(position: Int): Int {
-        val crime = crimeList[position]
-        return if (crime.isSerious) 1 else 0
+        holder.bind(crime)
     }
 
 }
